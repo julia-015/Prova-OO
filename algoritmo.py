0a6a315d72c7f978886cd6eac0839d75ea1c0441
julia-015 <jumd.vecchia@gmail.com>
2023-08-31 08:34:54 -0300

    update

--- algoritmo.py
+++ algoritmo.py
@@ -3,4 +3,34 @@ from Classes import *
 
 
 def main():
-    pass
+    
+    while True:
+        try:
+
+            print("Bem Vindo(a) a sua ToDoList! \n\n O que deseja realizar? \n\n [1] Adicionar Tarefa \n [2] Excluir Tarefa \n [3] Listar tarefas \n [4] Sair \n")
+            menu = int(input(">> "))
+
+
+            match menu:
+
+                case 1:
+                    pass
+
+                case 2:
+                    pass
+
+                case 3:
+                    pass
+
+                case 4:
+                    pass
+
+                case _:
+                    print("Valor Inválido.")
+                    print("---------------")
+
+
+        except Exception as erro:
+            print("Valor Inválido")
+            print(erro.__class__.__name__)
+            print("")
--- classes.py
+++ classes.py
@@ -1,13 +1,10 @@
 class ToDoList():
 
-    def __init__(self):
-        pass
-
     def adicionar_tarefa(self, descricao):
-        pass
+        self.descricao = descricao
 
     def excluir_tarefa(self, indice):
-        pass
+        self.indice = indice
 
     def listar_tarefas(self):
         pass
